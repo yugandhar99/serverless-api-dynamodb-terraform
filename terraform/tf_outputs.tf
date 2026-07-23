88444f1fb3648a2a909dcde73dd50816ef86f91b
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-07-23 14:32:25 -0500

    Update tf_outputs.tf

--- terraform/tf_outputs.tf
+++ terraform/tf_outputs.tf
@@ -3,7 +3,7 @@
 # ----------------------------------------------------------------------
 output "api_invoke_url" {
   value = aws_api_gateway_deployment.products_rest_api_deployment.invoke_url
-}
+} 
 
 # ----------------------------------------------------------------------
 # API key
